c0c1bf46baf2dfbf5e13e6486207083cb8d6dbf8
SeoSeunghun <proto_seo@naver.com>
2024-01-17 23:55:06 +0900

    feat: update input source setting component

--- src/main/kotlin/com/protoseo/inputsourceautoconverter/setting/InputSourceSettingComponent.kt
+++ src/main/kotlin/com/protoseo/inputsourceautoconverter/setting/InputSourceSettingComponent.kt
@@ -1,27 +1,46 @@
 package com.protoseo.inputsourceautoconverter.setting
 
 import javax.swing.JPanel
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.ui.ComboBox
+import com.intellij.ui.components.CheckBox
 import com.intellij.ui.components.JBLabel
 import com.intellij.util.ui.FormBuilder
 import com.protoseo.inputsourceautoconverter.utils.InputSourceUtils
 
 class InputSourceSettingComponent {
 
-    private val state: InputSourceSettingState = InputSourceSettingState.instance
-    val insertModeInputSourceComboBox = ComboBox(InputSourceUtils.getInputSources())
-    val notInsertModeInputSourceComboBox = ComboBox(InputSourceUtils.getInputSources())
-
-    val mainPanel: JPanel by lazy {
-        FormBuilder.createFormBuilder()
-            .addLabeledComponent(JBLabel("Insert mode input source"), insertModeInputSourceComboBox, 1)
-            .addLabeledComponent(JBLabel("Not Insert mode input source"), notInsertModeInputSourceComboBox, 2)
-            .addComponentFillVertically(JPanel(), 0)
-            .panel
-    }
+    val ideInitInputSourceComboBox = ComboBox(InputSourceUtils.getInputSources(), 450)
+    val normalModeInputSourceComboBox = ComboBox(InputSourceUtils.getInputSources(), 450)
+    val strictModeCheckBox = CheckBox("")
+    val mainPanel: JPanel = FormBuilder.createFormBuilder()
+        .addLabeledComponent(
+            createJBLabelWithToolTipText("IDE Init Input Source", "input source selected when IDE initialized"),
+            ideInitInputSourceComboBox
+        )
+        .addLabeledComponent(
+            createJBLabelWithToolTipText(
+                "Normal Mode(Not Insert Mode)",
+                "input sources selected in Not Insert Mode(COMMAND, VISUAL, SELECT, REPLACE, CMD_LINE)"
+            ),
+            normalModeInputSourceComboBox
+        )
+        .addLabeledComponent(
+            createJBLabelWithToolTipText("Auto Convert Strict Mode", "Can't change input source in not insert mode"),
+            strictModeCheckBox
+        )
+        .addSeparator()
+        .addComponentFillVertically(JPanel(), 0)
+        .panel
 
     init {
-        insertModeInputSourceComboBox.selectedItem = state.insertModeInputSource
-        notInsertModeInputSourceComboBox.selectedItem = state.notInsertModeInputSource
+        val state = ApplicationManager.getApplication().getService(InputSourceSettingState::class.java)
+        ideInitInputSourceComboBox.selectedItem = state.ideInitInputSource
+        normalModeInputSourceComboBox.selectedItem = state.normalModeInputSource
+    }
+
+    private fun createJBLabelWithToolTipText(text: String, toolTipText: String) = JBLabel().also {
+        it.text = text
+        it.toolTipText = toolTipText
     }
 }
