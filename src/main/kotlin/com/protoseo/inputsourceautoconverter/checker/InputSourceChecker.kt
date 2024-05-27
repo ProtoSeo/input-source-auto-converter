9235342dfb3471fec1ec2f6992889a066e414570
SeoSeunghun <proto_seo@naver.com>
2024-05-27 16:17:44 +0900

    refactor: convert to object class

--- src/main/kotlin/com/protoseo/inputsourceautoconverter/checker/InputSourceChecker.kt
+++ src/main/kotlin/com/protoseo/inputsourceautoconverter/checker/InputSourceChecker.kt
@@ -15,40 +15,37 @@ import com.protoseo.inputsourceautoconverter.common.InputSource
 import com.protoseo.inputsourceautoconverter.service.EditorVimModeService
 import com.protoseo.inputsourceautoconverter.setting.InputSourceSettingState
 
-class InputSourceChecker {
+object InputSourceChecker {
 
-    companion object {
-        fun check(project: Project): Boolean {
-            val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return false
-            val isStrictMode = service<InputSourceSettingState>().strictMode
-            val inputSource = service<InputSourceSettingState>().normalModeInputSource
+    fun check(project: Project): Boolean {
+        val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return false
+        val isStrictMode = service<InputSourceSettingState>().strictMode
+        val inputSource = service<InputSourceSettingState>().normalModeInputSource
 
-            if (isStrictMode) {
-                return isFocusCodeEditor(project) && isNotInsertMode(editor) && isNotSelectedInputSource(inputSource)
-            }
-
-            val editorVimModeService = project.service<EditorVimModeService>()
-            return (editorVimModeService.isFirstOpen(editor) && isNotSelectedInputSource(inputSource)) ||
-                    (editorVimModeService.isChangedVimMode(editor) && isNotSelectedInputSource(inputSource))
+        if (isStrictMode) {
+            return isFocusCodeEditor(project) && isNotInsertMode(editor) && isNotSelectedInputSource(inputSource)
         }
 
-        fun checkOnCapslock(project: Project): Boolean {
-            val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return false
-
-            return isFocusCodeEditor(project) && isNotInsertMode(editor) && Toolkit.getDefaultToolkit()
-                .getLockingKeyState(KeyEvent.VK_CAPS_LOCK)
-        }
-
-        private fun isNotSelectedInputSource(inputSource: InputSource): Boolean {
-            val locale = InputContext.getInstance().locale
-
-            return inputSource.language != locale.language && Locale.US.country != locale.country
-        }
-
-        private fun isFocusCodeEditor(project: Project) =
-            ToolWindowManager.getInstance(project).activeToolWindowId == null
-
-        private fun isNotInsertMode(editor: Editor) = CommandState.getInstance(editor).mode != Mode.INSERT
+        val editorVimModeService = project.service<EditorVimModeService>()
+        return (editorVimModeService.isFirstOpen(editor) && isNotSelectedInputSource(inputSource)) ||
+                (editorVimModeService.isChangedVimMode(editor) && isNotSelectedInputSource(inputSource))
     }
 
+    fun checkOnCapslock(project: Project): Boolean {
+        val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return false
+
+        return isFocusCodeEditor(project) && isNotInsertMode(editor) && Toolkit.getDefaultToolkit()
+            .getLockingKeyState(KeyEvent.VK_CAPS_LOCK)
+    }
+
+    private fun isNotSelectedInputSource(inputSource: InputSource): Boolean {
+        val locale = InputContext.getInstance().locale
+
+        return inputSource.language != locale.language && Locale.US.country != locale.country
+    }
+
+    private fun isFocusCodeEditor(project: Project) =
+        ToolWindowManager.getInstance(project).activeToolWindowId == null
+
+    private fun isNotInsertMode(editor: Editor) = CommandState.getInstance(editor).mode != Mode.INSERT
 }
