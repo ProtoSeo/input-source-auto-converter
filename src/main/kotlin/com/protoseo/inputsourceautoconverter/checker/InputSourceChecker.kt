f92e289a0d836612f4c804e1402d504f6cda3645
SeoSeunghun <proto_seo@naver.com>
2024-05-27 16:12:16 +0900

    refactor: extract logic for input source validate

---
+++ src/main/kotlin/com/protoseo/inputsourceautoconverter/checker/InputSourceChecker.kt
@@ -0,0 +1,45 @@
+package com.protoseo.inputsourceautoconverter.checker
+
+import java.awt.im.InputContext
+import java.util.Locale
+import com.intellij.openapi.components.service
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindowManager
+import com.maddyhome.idea.vim.command.CommandState
+import com.maddyhome.idea.vim.command.CommandState.Mode
+import com.protoseo.inputsourceautoconverter.common.InputSource
+import com.protoseo.inputsourceautoconverter.service.EditorVimModeService
+import com.protoseo.inputsourceautoconverter.setting.InputSourceSettingState
+
+class InputSourceChecker {
+
+    companion object {
+        fun check(project: Project): Boolean {
+            val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return false
+            val isStrictMode = service<InputSourceSettingState>().strictMode
+            val inputSource = service<InputSourceSettingState>().normalModeInputSource
+
+            if (isStrictMode) {
+                return isFocusCodeEditor(project) && isNotInsertMode(editor) && isNotSelectedInputSource(inputSource)
+            }
+
+            val editorVimModeService = project.service<EditorVimModeService>()
+            return (editorVimModeService.isFirstOpen(editor) && isNotSelectedInputSource(inputSource)) ||
+                    (editorVimModeService.isChangedVimMode(editor) && isNotSelectedInputSource(inputSource))
+        }
+
+        private fun isNotSelectedInputSource(inputSource: InputSource): Boolean {
+            val locale = InputContext.getInstance().locale
+
+            return inputSource.language != locale.language && Locale.US.country != locale.country
+        }
+
+        private fun isFocusCodeEditor(project: Project) =
+            ToolWindowManager.getInstance(project).activeToolWindowId == null
+
+        private fun isNotInsertMode(editor: Editor) = CommandState.getInstance(editor).mode != Mode.INSERT
+    }
+
+}
