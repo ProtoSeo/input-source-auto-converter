b67c34bc8c9c25b68b07647afb43237227be6490
SeoSeunghun <proto_seo@naver.com>
2024-07-16 22:42:32 +0900

    test: verify that it works well in other thread

--- src/test/kotlin/JNITest.kt
+++ src/test/kotlin/JNITest.kt
@@ -1,3 +1,7 @@
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.ThreadPoolExecutor.DiscardPolicy
+import java.util.concurrent.TimeUnit
 import org.junit.Test
 import com.protoseo.inputsourceautoconverter.converter.MacNative
 
@@ -12,5 +16,21 @@ class JNITest {
         MacNative.convert("com.apple.keylayout.ABC")
         Thread.sleep(500)
         MacNative.convert("com.apple.inputmethod.Korean.2SetKorean")
+    }
+
+    @Test
+    fun `다른 Thread 에서 실행해도 동작되는지 테스트`() {
+        val executor = ThreadPoolExecutor(
+            1, 1, Long.MAX_VALUE, TimeUnit.DAYS, ArrayBlockingQueue(10),
+            { r ->
+                val thread = Thread(r, "ideavim_extension")
+                thread.isDaemon = true
+                thread.priority = Thread.MAX_PRIORITY
+                thread
+            },
+            DiscardPolicy()
+        )
+
+        executor.execute { MacNative.convert("com.apple.inputmethod.Korean.2SetKorean") }
     }
 }
